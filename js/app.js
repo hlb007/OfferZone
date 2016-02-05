da47528bf8a2fec8b164936f2740f119297b4341
arbghl <l073776@AU21CALP3213.infau.wbcau.westpac.com.au>
2016-02-05 18:20:30 +1100

    Refactoring
    refactoring

--- js/app.js
+++ js/app.js
@@ -6,20 +6,14 @@ offerApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
 
     $stateProvider
-
-    // HOME STATES AND NESTED VIEWS ========================================
         .state('login', {
             url: '/login',
             templateUrl: 'views/login.html'
         })
-
-        // nested list with custom controller
         .state('old', {
             url: '/old',
             templateUrl: 'views/oldAge.html'
         })
-
-        // nested list with just some random string data
         .state('mid', {
             url: '/mid',
             templateUrl: 'views/middleAge.html'

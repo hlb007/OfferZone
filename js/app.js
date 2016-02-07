ce560704ad8a536d4ec15e58dd9c9e83d7527ee0
arbghl <b8.arun@gmail.com>
2016-02-07 18:12:26 +1100

    enabling location
    Enable the location service on the login page. Store the user selected
    behavior.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ angular.module('dynamic.form.module',[]);
 angular.module('offerApp', ['ui.router','dynamic.form.module']);
 angular.module('offerApp').constant('resourcePath', '/');
 angular.module('offerApp').constant('viewPath', 'views/');
+angular.module('offerApp').constant('geolocation_msgs', {
+        'errors.location.unsupportedBrowser':'Browser does not support location services',
+        'errors.location.permissionDenied':'You have rejected access to your location',
+        'errors.location.positionUnavailable':'Unable to determine your location',
+        'errors.location.timeout':'Service timeout has been reached'
+});
--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -3,12 +3,14 @@
     'use strict'
     angular.module('offerApp')
             .controller('LoginController',LoginController);
-            LoginController.$inject = ['$scope', 'viewPath', 'LoginService', '$state'];
-             function LoginController($scope, viewPath, LoginService, $state){
+            LoginController.$inject = ['$scope', 'viewPath', 'LoginService', '$state','geolocation','$rootScope'];
+             function LoginController($scope, viewPath, LoginService, $state, geolocation, $rootScope){
                 var vm = this;
                 vm.views = viewPath;
                 vm.errorClass = false;
                 vm.loginpage = true;
+                vm.showLocationMessage = true;
+                vm.locationMessage = 'We ask you to enable the location tracking to provide you better offers based on you current location.';
                 vm.sigIn = function(){
                   var loginPromise = LoginService.validateCreds(vm.username, vm.pass);
                   loginPromise.then(function(data){
@@ -22,6 +24,14 @@
                     vm.errorClass = true;
                   });
                 };
-
+                var geo = geolocation.getLocation();
+                geo.then(function(data){
+                  console.log('here'+data);
+                  vm.showLocationMessage = false
+                  vm.locationMessage = 'You have enabled the locaion tracking on this website. We will use this to customize your offers';
+                },function(data){
+                    console.log('there'+data);
+                    vm.locationMessage = data;
+                });
               }
   })();
